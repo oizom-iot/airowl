7c6961d39a6a1f55039bcdc44ec4a8af8451d261
Sohil Patel <sohilpatel4932@gmail.com>
2024-10-10 23:47:06 +0530

    Firmware folder clear

--- Firmware/replace_gdb.py
+++
@@ -1,8 +0,0 @@
-Import ("env")
-
-#env.Replace(GDB="/Users/mah/espressif-gdb/xtensa-esp-elf-gdb/bin/xtensa-esp32s3-elf-gdb")
-
-
-#env.Replace(GDB="/Users/mah/Downloads/xtensa-esp-elf-gdb/bin/xtensa-esp32s3-elf-gdb")
-#  "/Users/mah/Downloads/xtensa-esp-elf-gdb/bin",
-# toolchainBinDir
