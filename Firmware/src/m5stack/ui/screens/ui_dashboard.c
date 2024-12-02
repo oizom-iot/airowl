37b35248a3807398e326a7d6dc47b45877a9f9a0
Akrcode348 <akshaj.rgh@gmail.com>
2024-12-02 17:59:35 +0530

    Update ui_dashboard.c
    changed PPB label on dashboard screen to "Index"

--- Firmware/src/m5stack/ui/screens/ui_dashboard.c
+++ Firmware/src/m5stack/ui/screens/ui_dashboard.c
@@ -194,7 +194,7 @@ lv_obj_set_height( ui_TVOCppb, LV_SIZE_CONTENT);   /// 1
 lv_obj_set_x( ui_TVOCppb, -57 );
 lv_obj_set_y( ui_TVOCppb, 98 );
 lv_obj_set_align( ui_TVOCppb, LV_ALIGN_CENTER );
-lv_label_set_text(ui_TVOCppb,"ppb");
+lv_label_set_text(ui_TVOCppb,"Index");
 lv_obj_set_style_text_color(ui_TVOCppb, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT );
 lv_obj_set_style_text_opa(ui_TVOCppb, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
 lv_obj_set_style_text_font(ui_TVOCppb, &lv_font_montserrat_16, LV_PART_MAIN| LV_STATE_DEFAULT);
